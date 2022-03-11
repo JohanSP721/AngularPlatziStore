8c6ab35f4ce7b8d3190356ba5454f1623eed843b
Johan Sarmiento Peña <johansarmiento_21@live.com>
2022-03-11 16:22:08 -0500

    curso de fundamentos de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Product } from './Product.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularPlatziStore';
+export class AppComponent
+{
+	products: Product[] = [
+		{
+		  name: 'EL mejor juguete',
+		  price: 565,
+		  image: 'https://source.unsplash.com/random'
+		},
+		{
+		  name: 'Bicicleta casi nueva',
+		  price: 356,
+		  image: 'https://source.unsplash.com/random'
+		},
+		{
+		  name: 'Colleción de albumnes',
+		  price: 34,
+		  image: 'https://source.unsplash.com/random'
+		},
+		{
+		  name: 'Mis libros',
+		  price: 23,
+		  image: 'https://source.unsplash.com/random'
+		},
+		{
+		  name: 'Casa para perro',
+		  price: 34,
+		  image: 'https://source.unsplash.com/random'
+		},
+		{
+		  name: 'Gafas',
+		  price: 3434,
+		  image: 'https://source.unsplash.com/random'
+		}
+	  ]
 }
